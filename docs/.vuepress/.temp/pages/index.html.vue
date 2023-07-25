681fa5bccb2769bf00a34cdd04c690f84ba1e3bb
SingThai <93581386+singthaitan@users.noreply.github.com>
2023-07-26 01:45:38 +0800

    Docusaurus template site.

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,6 +0,0 @@
-<template><div><h1 id="charitable-index-page" tabindex="-1"><a class="header-anchor" href="#charitable-index-page" aria-hidden="true">#</a> CharitAble Index Page</h1>
-<h2 id="testing-markdown-scripts" tabindex="-1"><a class="header-anchor" href="#testing-markdown-scripts" aria-hidden="true">#</a> Testing markdown scripts</h2>
-<h3 id="testing" tabindex="-1"><a class="header-anchor" href="#testing" aria-hidden="true">#</a> Testing</h3>
-</div></template>
-
-
